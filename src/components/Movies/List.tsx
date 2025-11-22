3b292af91c47cde1416a6c83023b515b1debc042
Adam Smedberg <asmedberg@gmail.com>
2025-11-21 16:23:13 -0800

    update movie llist images

--- src/components/Movies/List.tsx
+++ src/components/Movies/List.tsx
@@ -20,11 +20,12 @@ export default function List({ movies, isLoading }: { movies: MovieProps[]; isLo
       {movies.map(m => (
         <div key={m.id} className={styles.listItem}>
           <Image
-            src={`https://image.tmdb.org/t/p/w500${m.poster_path}`}
+            src={`https://image.tmdb.org/t/p/w342${m.poster_path}`}
             alt={`${m.original_title} Poster`}
             width="500"
             height="750"
             title={m.original_title}
+            sizes="(max-width: 40rem) 50vw, (max-width: 48rem) 25vw, 16rem"
             className={styles.listImage}
           />
         </div>
