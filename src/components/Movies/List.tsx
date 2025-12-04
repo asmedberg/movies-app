86184835f20d4c64378e3949f8c3b37cb74763fd
Adam Smedberg <asmedberg@gmail.com>
2025-12-04 09:29:46 -0800

    persist search params when displaying movie details

--- src/components/Movies/List.tsx
+++ src/components/Movies/List.tsx
@@ -1,13 +1,21 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import type { MovieListProps } from "./types";
 import styles from "./movies.module.css";
 
 export default function List({ movies }: { movies: MovieListProps[] }) {
+  const searchParams = useSearchParams();
+
   return (
     <div className={styles.listLayout}>
       {movies.map(m => (
-        <Link key={m.id} href={`/movie/${m.id}`} className={styles.listItem}>
+        <Link
+          key={m.id}
+          href={`/movie/${m.id}${searchParams ? `?${searchParams.toString()}` : ""}`}
+          className={styles.listItem}
+        >
           <Image
             src={`https://image.tmdb.org/t/p/w342${m.poster_path}`}
             alt={`${m.title} Poster`}
